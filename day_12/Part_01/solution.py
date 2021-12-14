b3b8770685a090fd0fff400f9fa38199ef5b5276
Ruben van Meerten <ruben.vanmeerten@hotmail.nl>
2021-12-14 21:48:27 +0100

    started day 12

--- day_12/Part_01/solution.py
+++ day_12/Part_01/solution.py
@@ -5,15 +5,11 @@ for i in path_input:
 	paths.append(i.split('-'))
 
 for i in range(len(paths)):
-	if tunnels.get(paths[i][0]) is not None:
-		tunnels[(paths[i][0])].append(paths[i][1])
-	else:
+	if tunnels.get(paths[i][0]) is None:
 		tunnels[(paths[i][0])] = []
-		tunnels[(paths[i][0])].append(paths[i][1])
-	if tunnels.get(paths[i][1]) is not None:
-		tunnels[(paths[i][1])].append(paths[i][0])
-	else:
+	if tunnels.get(paths[i][1]) is None:
 		tunnels[(paths[i][1])] = []
-		tunnels[(paths[i][1])].append(paths[i][0])
+	tunnels[(paths[i][1])].append(paths[i][0])
+	tunnels[(paths[i][0])].append(paths[i][1])
 print(tunnels)
 ## tunnels shows a cave and the paths it has to another
