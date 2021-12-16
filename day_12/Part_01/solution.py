d1d3c8dab756ac98e294aa854e53ffc292425912
Ruben van Meerten <ruben.vanmeerten@hotmail.nl>
2021-12-16 17:44:53 +0100

    finished part 1 day 12

--- day_12/Part_01/solution.py
+++ day_12/Part_01/solution.py
@@ -1,15 +1,34 @@
-path_input = open("input.txt").read().splitlines()
-paths = []
-tunnels = dict()
-for i in path_input:
-	paths.append(i.split('-'))
+paths = [x.split('-') for x in open('input.txt', 'r').read().splitlines()]
+map_dict = dict()
+result = 0
+import copy
 
-for i in range(len(paths)):
-	if tunnels.get(paths[i][0]) is None:
-		tunnels[(paths[i][0])] = []
-	if tunnels.get(paths[i][1]) is None:
-		tunnels[(paths[i][1])] = []
-	tunnels[(paths[i][1])].append(paths[i][0])
-	tunnels[(paths[i][0])].append(paths[i][1])
-print(tunnels)
-## tunnels shows a cave and the paths it has to another
+for x in range(len(paths)):
+	if map_dict.get(paths[x][0]) is None:
+		map_dict[(paths[x][0])] = []
+	if map_dict.get(paths[x][1]) is None:
+		map_dict[(paths[x][1])] = []
+	if paths[x][1] != 'start':
+		map_dict[(paths[x][0])].append(paths[x][1])
+	if paths[x][0] != 'start':
+		map_dict[(paths[x][1])].append(paths[x][0])
+
+def depth_first_recursive(copy_of_map, current_step):
+	result = 0
+	if len(copy_of_map[current_step]) == 0:
+		return  0
+	if current_step == 'end':
+		return  1
+
+	new_copy_of_map = copy.deepcopy(copy_of_map)
+	if current_step.islower():
+		for step in new_copy_of_map:
+			if current_step in new_copy_of_map[step]:
+				new_copy_of_map[step].remove(current_step)
+
+	for i in range(len(new_copy_of_map[current_step])):
+		result += depth_first_recursive(new_copy_of_map, new_copy_of_map[current_step][i])
+	return result
+
+result += depth_first_recursive(map_dict, 'start')
+print('result:', result)
