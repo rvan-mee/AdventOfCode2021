ce2fb56d65f9f22e6169db88fbc065fd50a443b2
rvan-mee <Ruben@RubenDesktop>
2021-12-19 01:14:27 +0100

    finished day 13

--- day_13/Part_02/solution.py
+++ day_13/Part_02/solution.py
@@ -1,10 +1,10 @@
 from collections import defaultdict
 full_input = open("input.txt").read().split('\n\n')
 folds_input = full_input[1].split('\n')
-highest_x, highest_y, full_spots = 0, 0, 0
+highest_x, highest_y, = 0, 0
+paper = defaultdict(lambda: '  ')
 folds = []
 coords = []
-paper = defaultdict(lambda: '.')
 
 for x in full_input[0].split('\n'):
 	coords.append([int(i) for i in x.split(',')])
@@ -19,7 +19,7 @@ for x in coords:
 
 def set_coords():
 	for i in coords:
-		paper[i[1], i[0]] = '#'
+		paper[i[1], i[0]] = '██'
 
 def fold_paper(folding_line):
 	global paper
@@ -32,20 +32,18 @@ def fold_paper(folding_line):
 		highest_y = int(folding_line[1])
 	for y in range(highest_y + 1):
 		for x in range(highest_x + 1):
-			if fold == 'x' and paper[y, highest_x + x] == '#':
+			if fold == 'x' and paper[y, highest_x + x] == '██':
 				paper[y, highest_x - x] = paper[y, highest_x + x]
-			if fold == 'y' and paper[highest_y + y, x] == '#':
+			if fold == 'y' and paper[highest_y + y, x] == '██':
 				paper[highest_y - y, x] = paper[highest_y + y, x]
 
-def count():
-	global empty_spots
-	global full_spots
-	for y in range(highest_y + 1):
-		for x in range(highest_x + 1):
-			if paper[y, x] == '#':
-				full_spots += 1
+def print_paper():
+	for y in range(highest_y):
+		for x in range(highest_x):
+			print(paper[y, x], end="")
+		print()
 
 set_coords()
-fold_paper(folds[0])
-count()
-print('dots:', full_spots)
+for i in range(len(folds)):
+	fold_paper(folds[i])
+print_paper()
